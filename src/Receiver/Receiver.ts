7978eb0771e6578b65df6bc2440fb88d8dbe36a0
Luca Scaringella <lucalstyle@gmail.com>
2021-03-30 22:36:53 +0200

    Implement receiver register.

---
+++ src/Receiver/Receiver.ts
@@ -0,0 +1,57 @@
+/*
+Author: Luca Scaringella
+GitHub: LucaCode
+Copyright(c) Luca Scaringella
+ */
+
+import { AGServerSocket } from "socketcluster-server";
+import { EndpointClass, parseEndpointName } from "../Endpoint";
+import { endpointAppliers } from "../Endpoints";
+import { createMiddlewarePipeline } from "../Middleware/MiddlewarePipeline";
+import ScTransmitRequest from "./ScTransmitRequest";
+
+export interface Receiver {
+  /**
+   * @description
+   * Methods that handle requests after processing the middlewares.
+   */
+  handle: (socket: AGServerSocket, data: any) => any;
+}
+
+export default function RegisterReceiver(name?: string) {
+  return (target: EndpointClass<Receiver>) => {
+    const endpointName = name != null ? name : parseEndpointName(target);
+    if (target.registered)
+      throw new Error(`The receiver: "${endpointName}" is already registered.`);
+    target.registered = true;
+
+    const instance = new target();
+    const middlewarePipeline = createMiddlewarePipeline(
+      target.middlewares ? [...target.middlewares].reverse() : []
+    );
+
+    endpointAppliers.push((socket) => {
+      (async () => {
+        let request: ScTransmitRequest;
+        for await (request of socket.receiver(endpointName)) {
+          try {
+            await middlewarePipeline(socket, request.data, undefined);
+          } catch (err) {
+            console.error(
+              `Unexpected error in middleware pipeline of receiver: '${endpointName}' -> `,
+              err
+            );
+          }
+          try {
+            await instance.handle(socket, request.data);
+          } catch (err) {
+            console.error(
+              `Unexpected error in handle of receiver: '${endpointName}' -> `,
+              err
+            );
+          }
+        }
+      })();
+    });
+  };
+}
